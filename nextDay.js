487015afe373f365c517cb2c16f2e7c77d52a90d
schlezes <89032071+schlezes@users.noreply.github.com>
2021-10-28 08:30:23 -0700

    Create nextDay.js
    I am working on a simple mobile app.  
    I needed to present the next few days in sequence into a UL/LI element.
    The number of LI elements maybe fixed or unknown.
    I am using the Date() method for obtaining the current date.
    Then calling this function to obtain the subsequent next day following today's day.
    Then use this function as part of a forEach method to populate the HTMLElements object:  {  "0": {}, "1": {},  "2": {},  "3": {},  "4": {} }

---
+++ nextDay.js
@@ -0,0 +1,20 @@
+// this is for going forward
+// both r === 6 and ary[r + 1]
+// work together
+
+// when r === 6
+// the next day has to be sun
+
+var ary = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var nextX = function (x) {
+    var result;
+    var r = ary.indexOf(x);
+    if (r === 6) {
+        result = "Sun"; // next day has to be sun
+    }
+    else {
+        result = ary[r + 1]; // okay to get next day
+    }
+    return result;
+};
+console.log(nextX("Sat"));
